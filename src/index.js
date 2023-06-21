75cf23dd0861dc63d0f4a0b48cda2140bfcd2be0
enzo.villanueva <enzo.villanueva@pragma.com.co>
2023-06-21 17:02:16 -0500

    xray

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const AWSXRay = require('aws-xray-sdk')
 AWSXRay.captureHTTPsGlobal(require('http'))
 AWSXRay.captureHTTPsGlobal(require('https'))
 
+app.use(AWSXRay.express.openSegment('message-microservice-v1'))
+
 const serverless = require("serverless-http")
 const express = require("express");
 const app = express();
@@ -11,5 +13,7 @@ const routes = require("./routes/message.routes");
 app.use(express.json());
 
 app.use("/api/v1", routes);
+
+app.use(AWSXRay.express.closeSegment())
 
 module.exports.handler = serverless(app);
